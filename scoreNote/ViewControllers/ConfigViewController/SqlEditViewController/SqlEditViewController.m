baa3e2b4bf2e0be44e78fb33d6792c7c49cf9274
gejunyu27 <393864523@qq.com>
2026-03-08 21:23:40 +0800

    ios26适配:数据库页面tableview高度

--- scoreNote/ViewControllers/ConfigViewController/SqlEditViewController/SqlEditViewController.m
+++ scoreNote/ViewControllers/ConfigViewController/SqlEditViewController/SqlEditViewController.m
@@ -297,7 +297,12 @@ typedef NS_ENUM(NSInteger, SqlRecordType) {
 - (UITableView *)tableView
 {
     if (!_tableView) {
-        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - NAV_BAR_HEIGHT)];
+        CGFloat h = SCREEN_HEIGHT - NAV_BAR_HEIGHT;
+        if (@available(iOS 15.0, *)) { //适配ios26
+            h = SCREEN_HEIGHT;
+        }
+        
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, h)];
         _tableView.delegate = self;
         _tableView.dataSource = self;
         //        _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
